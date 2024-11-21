98da9783336f3bdbe56cad5c6f31cc8f07c81558
Zeyad Issa <zeyadissa@Zeyads-Macbook-Pro.local>
2024-11-21 19:52:05 +0000

    Initial

---
+++ const/glob.R
@@ -0,0 +1,2 @@
+#Add your global variables here. These include links, constant variables,
+#Or any fixed output that you may wish to change or vary
---
+++ src/functions.R
@@ -0,0 +1,3 @@
+#Add your functions here. Note that functions need to follow standard conventions
+#Try to ensure your functions are well documented, ideally with proper tags
+#See tHF style guide for further detail [INSERT LINK HERE]
